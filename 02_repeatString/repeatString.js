dd78e4eebaa92b924cc58d64d7aa88fe2450ac8d
maikol9703 <maikolcristhian1997@gmail.com>
2023-04-25 17:20:41 -0600

    Solve exercise using for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if (num===-1){
+        return "ERROR";
+    }
+    let finString="";
+    for (let i=0;i<num;i++){
+        finString+=`${string}`;
+    }
+    return finString;
 };
+
+repeatString("hey",3)
 
 // Do not edit below this line
 module.exports = repeatString;
