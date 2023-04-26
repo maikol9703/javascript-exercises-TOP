fb2be19a9fd4451fdaac29d8823f8c9a3b0db05c
maikol9703 <maikolcristhian1997@gmail.com>
2023-04-26 00:46:10 -0600

    Solve proble with conditionals and for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    
+    if (a>b){
+        let temp=a;
+        a=b;
+        b=temp;
+    }
+    if ((a <0 || b <0) || (isNaN(a) || isNaN(b)) || (typeof a ==="string" || typeof b === "string")){
+        return "ERROR"
+    }
+    total=0;
+    for (let i=a;i<=b;i++){
+        total+=i;
+    }
+    return total;
 
 };
 
