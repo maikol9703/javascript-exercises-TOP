f6b4303a8d5698901894f462dc4fd94459b739f2
maikol9703 <maikolcristhian1997@gmail.com>
2023-04-25 18:27:39 -0600

    Solve the problem with split and for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let sliceStr=string.split("");
+    let stringRev="";
+    for (let i= string.length - 1;i>=0;i--){
+        stringRev+=`${sliceStr[i]}`;
+    }
+    return stringRev;
 
 };
 
