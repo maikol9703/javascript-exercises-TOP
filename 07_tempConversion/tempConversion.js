5ec588a00576676623abcd775656afbdcce32ad0
maikol9703 <maikolcristhian1997@gmail.com>
2023-04-26 18:49:39 -0600

    Solve with Math.round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  newFar=(far - 32)* (5/9);
+  return (Math.round(newFar * 10))/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  newCel=(cel*(9/5))+32;
+  return (Math.round(newCel * 10))/10;
 };
 
 // Do not edit below this line
